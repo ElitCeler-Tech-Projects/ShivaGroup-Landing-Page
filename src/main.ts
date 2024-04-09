fd1f29122fb88dec6d27d68d8e9526431653f1af
Nagateja Valiveti <teja91.valiveti@gmail.com>
2024-04-09 21:58:34 +0530

    landing page

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import './style.css';
+
+const sidebarBtn = document.querySelector('#sidebarBtn') as HTMLUListElement;
+const sidebarMenu = document.querySelector('#sidebarMenu') as HTMLUListElement;
+
+const navbarMenuClasses = "flex menu-vertical absolute top-[100%] right-0 p-2 bg-base-300 shadow-lg px-1 z-[100]"
+
+function toggleNavbar() {
+  sidebarMenu.classList.toggle('hidden');
+  sidebarMenu.classList.toggle('menu-horizontal');
+  
+  navbarMenuClasses.split(' ').forEach((cls: string) => {
+    sidebarMenu.classList.toggle(cls);
+  })
+}
+
+sidebarBtn.addEventListener('click', toggleNavbar);
