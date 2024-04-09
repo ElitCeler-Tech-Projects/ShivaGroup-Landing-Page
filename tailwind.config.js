600abad320586d2ab6854c045f27c17e644c4f50
Nagateja Valiveti <teja91.valiveti@gmail.com>
2024-04-09 22:18:38 +0530

    UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "acid", "forest"],
+    themes: ["light", "dark", "cupcake"],
   }
 }
 
